87c30b1c7dfeaac34c4c4bc522f9ecdba1551793
Weiss Bai <whitesgr03@gmail.com>
2023-02-09 14:14:27 +0800

    Refactor create ship with id parameter

--- src/components/ships.test.js
+++ src/components/ships.test.js
@@ -2,11 +2,9 @@ import { createShip, SHIP_LIST } from "./handleShip";
 
 describe("createShip() Return objects should include name, length, hits sunk", () => {
     test("create 'Patrol Boat'", () => {
-        const patrol_boat = SHIP_LIST.find(
-            (ship) => ship.name === "Patrol Boat"
-        );
+        const shipId = SHIP_LIST.find((item) => "Patrol Boat" === item.name).id;
 
-        const actual = createShip(patrol_boat);
+        const actual = createShip(shipId);
 
         const expected = {
             name: "Patrol Boat",
@@ -18,9 +16,9 @@ describe("createShip() Return objects should include name, length, hits sunk", (
         expect(actual).toEqual(expected);
     });
     test("create 'Carrier'", () => {
-        const carrier = SHIP_LIST.find((ship) => ship.name === "Carrier");
+        const shipId = SHIP_LIST.find((item) => "Carrier" === item.name).id;
 
-        const actual = createShip(carrier);
+        const actual = createShip(shipId);
 
         const expected = {
             name: "Carrier",
@@ -43,24 +41,12 @@ describe("createShip() Return objects should include name, length, hits sunk", (
 
         expect(actual).toEqual(expected);
     });
-    test("Input incorrect object (1)", () => {
-        const ship = SHIP_LIST.find((ship) => ship.name === "Boat");
+    test("Input incorrect param", () => {
+        const shipId = SHIP_LIST.find((item) => "Boat" === item.id);
+        const shipId2 = SHIP_LIST.find((item) => 6 === item.id);
+        const shipId3 = SHIP_LIST.find((item) => -1 === item.id);
 
-        const actual = createShip(ship);
-
-        const expected = {
-            name: "Patrol Boat",
-            length: 2,
-            hits: 0,
-            sunk: false,
-        };
-
-        expect(actual).toEqual(expected);
-    });
-    test("Input incorrect object (2)", () => {
-        const actual = createShip({
-            name: "Boat",
-        });
+        const actual = createShip(shipId);
 
         const expected = {
             name: "Patrol Boat",
@@ -69,17 +55,19 @@ describe("createShip() Return objects should include name, length, hits sunk", (
             sunk: false,
         };
 
+        const second = createShip(shipId2);
+        const third = createShip(shipId3);
+
         expect(actual).toEqual(expected);
+        expect(second).toEqual(expected);
+        expect(third).toEqual(expected);
     });
 });
 
 describe("increaseHit()", () => {
     test("should increase hit", () => {
-        const ship = createShip({
-            id: 2,
-        });
-
-        ship.increaseHit();
+        const shipId = SHIP_LIST.find((item) => "Patrol Boat" === item.name).id;
+        const ship = createShip(shipId);
 
         const actual = ship.hits;
         const expected = 1;
@@ -87,9 +75,8 @@ describe("increaseHit()", () => {
         expect(actual).toEqual(expected);
     });
     test("should add two hits", () => {
-        const ship = createShip({
-            id: 2,
-        });
+        const shipId = SHIP_LIST.find((item) => "Patrol Boat" === item.name).id;
+        const ship = createShip(shipId);
 
         ship.increaseHit();
         ship.increaseHit();
@@ -103,8 +90,9 @@ describe("increaseHit()", () => {
 
 describe("isSunk()", () => {
     test("should check if length and hit are equal (1)", () => {
-        const submarine = SHIP_LIST.find((ship) => ship.name === "Submarine");
-        const ship = createShip(submarine);
+        const shipId = SHIP_LIST.find((item) => "Patrol Boat" === item.name).id;
+
+        const ship = createShip(shipId);
 
         const actual = ship.isSunk();
         const expected = false;
@@ -112,8 +100,9 @@ describe("isSunk()", () => {
         expect(actual).toEqual(expected);
     });
     test("should check if length and hit are equal (2)", () => {
-        const submarine = SHIP_LIST.find((ship) => ship.name === "Submarine");
-        const ship = createShip(submarine);
+        const shipId = SHIP_LIST.find((item) => "Submarine" === item.name).id;
+
+        const ship = createShip(shipId);
 
         ship.increaseHit();
         ship.increaseHit();
