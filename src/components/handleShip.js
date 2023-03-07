441127b96907c9fc19253d45c90986c4ea1cd4bf
Weiss Bai <whitesgr03@gmail.com>
2023-03-07 11:52:03 +0800

    Refactor setPos Call createShipAllPos to get the full coordinate position of the ship

--- src/components/handleShip.js
+++ src/components/handleShip.js
@@ -23,6 +23,42 @@ const setProto = (ship) => {
     let pos = [];
     let centerPos = [];
 
+    const createShipAllPos = (name, [x, y], isShipRotate) => {
+        const shipPos = [[x, y]];
+
+        if (isShipRotate) {
+            if (name === "Carrier") {
+                shipPos.push([x, y - 2]);
+            }
+
+            if (name === "Battleship") {
+                shipPos.push([x, y + 2]);
+            }
+
+            if (name !== "Patrol Boat") {
+                shipPos.push([x, y - 1]);
+            }
+
+            shipPos.push([x, y + 1]);
+        } else {
+            if (name === "Carrier") {
+                shipPos.push([x + 2, y]);
+            }
+
+            if (name === "Battleship") {
+                shipPos.push([x - 2, y]);
+            }
+
+            if (name !== "Patrol Boat") {
+                shipPos.push([x + 1, y]);
+            }
+
+            shipPos.push([x - 1, y]);
+        }
+
+        return shipPos;
+    };
+
     return {
         increaseHit() {
             return (hits += 1);
@@ -30,8 +66,13 @@ const setProto = (ship) => {
         isSunk() {
             return hits === length;
         },
-        setPos(position) {
-            pos = position;
+        setPos(position, isShipRotate) {
+            if (position.length === 2) {
+                pos = createShipAllPos(this.name, position, isShipRotate);
+            } else {
+                pos = [];
+            }
+
             return [...pos];
         },
         getPos() {
