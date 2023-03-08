1cc14ba19c738b20760f5ce030d3765fdf292e42
Weiss Bai <whitesgr03@gmail.com>
2023-03-08 15:31:11 +0800

    Add computer

--- src/components/handlePlayer.js
+++ src/components/handlePlayer.js
@@ -35,4 +35,85 @@ const origin = () => {
     };
 };
 
+const computer = () => {
+    let possiblePos = [];
+    let previousHitPos = null;
+
+    return {
+        setPreviousHitPos(position) {
+            previousHitPos = position;
+
+            return this.getPreviousHitPos();
+        },
+        getPreviousHitPos() {
+            if (!previousHitPos) return false;
+
+            return [previousHitPos[0], previousHitPos[1]];
+        },
+        setPossiblePos(position) {
+            possiblePos = position;
+            return [...possiblePos];
+        },
+        getPossiblePos() {
+            return [...possiblePos];
+        },
+        calcPossiblePos(position) {
+            const array = [];
+            const [x, y] = position;
+
+            if (x + 1 <= 10) {
+                array.push([x + 1, y]);
+            }
+
+            if (x - 1 >= 1) {
+                array.push([x - 1, y]);
+            }
+
+            if (y + 1 <= 10) {
+                array.push([x, y + 1]);
+            }
+
+            if (y - 1 >= 1) {
+                array.push([x, y - 1]);
+            }
+
+            return array;
+        },
+        filterPossiblePos(current) {
+            const previous = previousHitPos;
+            const possible = [...possiblePos];
+
+            let arr = [];
+
+            if (previous[0] === current[0]) {
+                arr = possible.filter(([x]) => x === previous[0]);
+
+                if (current[1] > previous[1]) {
+                    if (current[1] + 1 <= 10) {
+                        arr.push([current[0], current[1] + 1]);
+                    }
+                } else {
+                    if (current[1] - 1 >= 1) {
+                        arr.push([current[0], current[1] - 1]);
+                    }
+                }
+            } else {
+                arr = possible.filter(([, y]) => y === previous[1]);
+
+                if (current[0] > previous[0]) {
+                    if (current[0] + 1 <= 10) {
+                        arr.push([current[0] + 1, current[1]]);
+                    }
+                } else {
+                    if (current[0] - 1 >= 1) {
+                        arr.push([current[0] - 1, current[1]]);
+                    }
+                }
+            }
+
+            return arr;
+        },
+    };
+};
+
 export { createPlayer };
